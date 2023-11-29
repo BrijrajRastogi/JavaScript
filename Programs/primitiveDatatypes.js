c8ff6c03b30f741304d65d25903d7b5ad4313783
BrijrajRastogi <123160947+BrijrajRastogi@users.noreply.github.com>
2023-11-29 16:36:34 +0530

    Add files via upload

---
+++ Programs/primitiveDatatypes.js
@@ -0,0 +1,29 @@
+//Number
+let age = 21;
+console.log(age + ": is a number.");
+
+//String
+let myName = "Brijraj";
+console.log(myName + ": is a string.");
+
+//Boolean
+let isStudent = true;
+let isMarried = false;
+console.log(isStudent + ": is a boolean.");
+console.log(isMarried + ": is a boolean.");
+
+//Undefined
+let a;
+console.log(a);
+
+//Null 
+let x = null;
+console.log(x);
+
+//BigInt
+let number = BigInt("1234567890");
+console.log(number);
+
+//Symbol
+let y = Symbol("Hello!");
+console.log(y);
