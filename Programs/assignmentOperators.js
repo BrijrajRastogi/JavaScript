57ffb55df83d5a9eb680ac7c6751d23b9e56c8b8
BrijrajRastogi <123160947+BrijrajRastogi@users.noreply.github.com>
2023-12-07 11:42:09 +0530

    Add files via upload

---
+++ Programs/assignmentOperators.js
@@ -0,0 +1,53 @@
+//Assignment operator(=)
+let a = 2;
+let b = 3;
+console.log("a =",a); //printing the value of a
+console.log("b =",b); //printing the value of b
+
+a = b; //assigning the value of b to a
+console.log("a = b:",a); //printing updated value of a
+
+b = a; //assigning the value of a to b
+console.log("b = a:",b); //printing updated value of b
+
+
+//Addition assignment operator(+=)
+let c = 1;
+console.log("c =",c);
+c += 5; //it is the same as "c = c + 5"
+console.log("c += 5:",c); //printing the updated value of c
+
+
+//Subtraction assignment operator(-=)
+let d = 5;
+console.log("d =",d); //printing the value of d
+d -= 1; //it is the same as "d = d - 1" 
+console.log("d -= 1:",d);
+
+
+//Multiplication assignment operator(*=)
+let e = 5;
+console.log("e =",e); //printing the value of e
+e *= 2; //it is the same as "e = e * 2"
+console.log("e *= 2:",e); //printing the updated value of e
+
+
+//Division assignment operator(/=)
+let f = 5;
+console.log("f =",f); //printing the value of f
+f /= 4; //it is the same as "f = f / 4"
+console.log("f /= 4:",f); //printing the updated value of f
+
+
+//Modulus assignment operator(%=)
+let g = 5;
+console.log("g =", 5); //printing the value of g
+g %= 4; //it is the same as "g = g % 4"
+console.log("g %= 4:",g); //printing the updated value of g
+
+
+//Exponential assignment operator(**=)
+let h = 5;
+console.log("h =",h); //printing the value of h
+h **= 4; //it is the same as "h = h ** 4"
+console.log("h **= 4:",h); //printing the updated value of h
